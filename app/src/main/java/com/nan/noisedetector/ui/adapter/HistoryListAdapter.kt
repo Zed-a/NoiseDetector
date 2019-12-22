5e5a1dd904aa4e92236b28b8c20c649c77ee5969
nan <1289704070@qq.com>
2019-12-22 22:02:57 +0800

    优化历史记录的UI布局

--- app/src/main/java/com/nan/noisedetector/ui/adapter/HistoryListAdapter.kt
+++ app/src/main/java/com/nan/noisedetector/ui/adapter/HistoryListAdapter.kt
@@ -3,14 +3,22 @@ package com.nan.noisedetector.ui.adapter
 import android.annotation.SuppressLint
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 import com.nan.noisedetector.R
 import com.nan.noisedetector.bean.HistoryData
 import com.nan.noisedetector.util.PreferenceHelper.historyRecord
+
 
 class HistoryListAdapter(private var mData: ArrayList<HistoryData>, private val context: Context) :
         RecyclerView.Adapter<HistoryListAdapter.ViewHolder>() {
@@ -23,12 +31,28 @@ class HistoryListAdapter(private var mData: ArrayList<HistoryData>, private val 
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         with(mData[position]) {
-            holder.time.text = "$date  $time"
+            holder.time.text = "$date    $time"
             holder.position.text = location
-            holder.maxDecibel.text = max.toString()
-            holder.averageDecibel.text = average.toString()
+            holder.position.setOnLongClickListener {
+                if (holder.position.layout.getEllipsisCount(0) != 0) {
+                    Toast.makeText(context, holder.position.text, LENGTH_LONG).show()
+                    true
+                } else false
+            }
             holder.item.setOnClickListener {}
             holder.deleteView.setOnClickListener { delete(position) }
+
+            var spanString = SpannableStringBuilder("最大分贝 $max")
+            spanString.setSpan(ForegroundColorSpan(context.getColor(R.color.colorPrimary)), 0, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            spanString.setSpan(AbsoluteSizeSpan(14, true), 0, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            holder.maxDecibel.text = spanString
+
+            spanString = SpannableStringBuilder("平均分贝 $average")
+            spanString.setSpan(ForegroundColorSpan(context.getColor(R.color.colorPrimary)), 0, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            spanString.setSpan(AbsoluteSizeSpan(10, true), 0, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            holder.averageDecibel.text = spanString
+
+
         }
     }
 
